5f4fb1ff2ab7eca24bfd4728baf116b953bc382d
Sveinung Himle <himsve@kartverket.no>
2021-12-17 09:40:35 +0100

    Add files

---
+++ Behavioral/Observer.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer.RealWorld
+{
+    /// <summary>
+    /// Observer Design Pattern
+    /// </summary>
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create IBM stock and attach investors
+            IBM ibm = new IBM("IBM", 120.00);
+            ibm.Attach(new Investor("Sorros"));
+            ibm.Attach(new Investor("Berkshire"));
+
+            // Fluctuating prices will notify investors
+            ibm.Price = 120.10;
+            ibm.Price = 121.00;
+            ibm.Price = 120.50;
+            ibm.Price = 120.75;
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    public abstract class Stock
+    {
+        private string symbol;
+        private double price;
+        private List<IInvestor> investors = new List<IInvestor>();
+
+        // Constructor
+        public Stock(string symbol, double price)
+        {
+            this.symbol = symbol;
+            this.price = price;
+        }
+
+        public void Attach(IInvestor investor)
+        {
+            investors.Add(investor);
+        }
+
+        public void Detach(IInvestor investor)
+        {
+            investors.Remove(investor);
+        }
+
+        public void Notify()
+        {
+            foreach (IInvestor investor in investors)
+            {
+                investor.Update(this);
+            }
+
+            Console.WriteLine("");
+        }
+
+        // Gets or sets the price
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (price != value)
+                {
+                    price = value;
+                    Notify();
+                }
+            }
+        }
+
+        // Gets the symbol
+        public string Symbol
+        {
+            get { return symbol; }
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteSubject' class
+    /// </summary>
+    public class IBM : Stock
+    {
+        // Constructor
+        public IBM(string symbol, double price)
+            : base(symbol, price)
+        {
+        }
+    }
+
+    /// <summary>
+    /// The 'Observer' interface
+    /// </summary>
+    public interface IInvestor
+    {
+        void Update(Stock stock);
+    }
+
+    /// <summary>
+    /// The 'ConcreteObserver' class
+    /// </summary>
+    public class Investor : IInvestor
+    {
+        private string name;
+        private Stock stock;
+
+        // Constructor
+        public Investor(string name)
+        {
+            this.name = name;
+        }
+
+        public void Update(Stock stock)
+        {
+            Console.WriteLine("Notified {0} of {1}'s " +
+                "change to {2:C}", name, stock.Symbol, stock.Price);
+        }
+
+        // Gets or sets the stock
+        public Stock Stock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+    }
+}
+
+namespace Observer.Structural
+{
+    /// <summary>
+    /// Observer Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Configure Observer pattern
+            ConcreteSubject s = new ConcreteSubject();
+
+            s.Attach(new ConcreteObserver(s, "Ove"));
+            s.Attach(new ConcreteObserver(s, "Sveinung"));
+            s.Attach(new ConcreteObserver(s, "Karoline"));
+
+            // Change subject and notify observers
+            s.SubjectState = "Hans Sverre er på jobb";
+            s.Notify();
+
+            s.SubjectState = "Hans Sverre har logga ut";
+            s.Notify();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    public abstract class Subject
+    {
+        private List<Observer> observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer o in observers)
+            {
+                o.Update();
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteSubject' class
+    /// </summary>
+    public class ConcreteSubject : Subject
+    {
+        private string subjectState;
+
+        // Gets or sets subject state
+        public string SubjectState
+        {
+            get { return subjectState; }
+            set { subjectState = value; }
+        }
+    }
+
+    /// <summary>
+    /// The 'Observer' abstract class
+    /// </summary>
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    /// <summary>
+    /// The 'ConcreteObserver' class
+    /// </summary>
+    public class ConcreteObserver : Observer
+    {
+        private string name;
+        private string observerState;
+        private ConcreteSubject subject;
+
+        // Constructor
+        public ConcreteObserver(
+            ConcreteSubject subject, string name)
+        {
+            this.subject = subject;
+            this.name = name;
+        }
+
+        public override void Update()
+        {
+            observerState = subject.SubjectState;
+            Console.WriteLine("Observer {0}'s new state is {1}",
+                name, observerState);
+        }
+
+        // Gets or sets subject
+        public ConcreteSubject Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+    }
+}
