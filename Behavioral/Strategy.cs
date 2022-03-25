4f263b66edc7f9e91db615f3be569c836cf86493
Sveinung Himle <himsve@kartverket.no>
2022-03-25 10:21:11 +0100

    Strategy pattern

---
+++ Behavioral/Strategy.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.RealWorld
+{
+    /// <summary>
+    /// Strategy Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Two contexts following different strategies
+
+            SortedList studentRecords = new SortedList();
+
+            studentRecords.Add("Åsmund");
+            studentRecords.Add("Hans Sverre");
+            studentRecords.Add("Ove");
+            studentRecords.Add("Arnlaug");
+            studentRecords.Add("Store-Knut");
+            studentRecords.Add("Vetle-Knut");
+
+            studentRecords.SetSortStrategy(new QuickSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new ShellSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new MergeSort());
+            studentRecords.Sort();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Strategy' abstract class
+    /// </summary>
+    public abstract class SortStrategy
+    {
+        public abstract void Sort(List<string> list);
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+
+    public class QuickSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            list.Sort();  // Default is Quicksort
+            Console.WriteLine("QuickSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+
+    public class ShellSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            Console.WriteLine("Ikkje implementert. Ring Kundeservice!");
+            Console.WriteLine("ShellSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteStrategy' class
+    /// </summary>
+
+    public class MergeSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            Console.WriteLine("Ikkje implementert. Ring Helpdesk!");
+            Console.WriteLine("MergeSorted list ");
+        }
+    }
+
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    public class SortedList
+    {
+        private List<string> list = new List<string>();
+        private SortStrategy sortstrategy;
+
+        public void SetSortStrategy(SortStrategy sortstrategy)
+        {
+            this.sortstrategy = sortstrategy;
+        }
+
+        public void Add(string name)
+        {
+            list.Add(name);
+        }
+
+        public void Sort()
+        {
+            sortstrategy.Sort(list);
+
+            // Iterate over list and display results
+
+            foreach (string name in list)             
+                Console.WriteLine(" " + name);
+            
+            Console.WriteLine();
+        }         
+    }
+}
+
