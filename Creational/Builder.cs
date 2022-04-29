62c9c8f112dabc5cf7fafd2441772454dad1e869
Sveinung Himle <himsve@kartverket.no>
2022-04-29 11:57:51 +0200

    Builder added

---
+++ Creational/Builder.cs
@@ -0,0 +1,438 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Creational
+{
+    // The Builder interface specifies methods for creating the different parts
+    // of the Product objects.     
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+
+    // The Concrete Builder classes follow the Builder interface and provide
+    // specific implementations of the building steps. Your program may have
+    // several variations of Builders, implemented differently.      
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new Product();
+
+        // A fresh builder instance should contain a blank product object, which
+        // is used in further assembly.         
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this._product = new Product();
+        }
+
+        // All production steps work with the same product instance.
+        public void BuildPartA()
+        {
+            this._product.Add("PartA1");
+        }
+
+        public void BuildPartB()
+        {
+            this._product.Add("PartB1");
+        }
+
+        public void BuildPartC()
+        {
+            this._product.Add("PartC1");
+        }
+
+        // Concrete Builders are supposed to provide their own methods for
+        // retrieving results. That's because various types of builders may
+        // create entirely different products that don't follow the same
+        // interface. Therefore, such methods cannot be declared in the base
+        // Builder interface (at least in a statically typed programming
+        // language).
+        //
+        // Usually, after returning the end result to the client, a builder
+        // instance is expected to be ready to start producing another product.
+        // That's why it's a usual practice to call the reset method at the end
+        // of the `GetProduct` method body. However, this behavior is not
+        // mandatory, and you can make your builders wait for an explicit reset
+        // call from the client code before disposing of the previous result.
+        public Product GetProduct()
+        {
+            Product result = this._product;
+
+            this.Reset();
+
+            return result;
+        }
+    }
+
+    // It makes sense to use the Builder pattern only when your products are
+    // quite complex and require extensive configuration.
+    //
+    // Unlike in other creational patterns, different concrete builders can
+    // produce unrelated products. In other words, results of various builders
+    // may not always follow the same interface.
+    public class Product
+    {
+        private List<object> _parts = new List<object>();
+
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for (int i = 0; i < this._parts.Count; i++)
+            {
+                str += this._parts[i] + ", ";
+            }
+
+            str = str.Remove(str.Length - 2); // removing last ",c"
+
+            return "Product parts: " + str + "\n";
+        }
+    }
+
+    // The Director is only responsible for executing the building steps in a
+    // particular sequence. It is helpful when producing products according to a
+    // specific order or configuration. Strictly speaking, the Director class is
+    // optional, since the client can control builders directly.
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+
+        // The Director can construct several product variations using the same
+        // building steps.
+        public void BuildMinimalViableProduct()
+        {
+            this._builder.BuildPartA();
+        }
+
+        public void BuildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code creates a builder object, passes it to the
+            // director and then initiates the construction process. The end
+            // result is retrieved from the builder object.
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+
+            Console.WriteLine("Standard basic product:");
+            director.BuildMinimalViableProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standard full featured product:");
+            director.BuildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            // Remember, the Builder pattern can be used without a Director
+            // class.
+            Console.WriteLine("Custom product:");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.Write(builder.GetProduct().ListParts());
+        }
+    }
+}
+
+namespace BuilderDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Client Code
+            Report report;
+            ReportDirector reportDirector = new ReportDirector();
+            // Construct and display Reports
+            PDFReport pdfReport = new PDFReport();
+            report = reportDirector.MakeReport(pdfReport);
+            report.DisplayReport();
+            Console.WriteLine("-------------------");
+            ExcelReport excelReport = new ExcelReport();
+            report = reportDirector.MakeReport(excelReport);
+            report.DisplayReport();
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class ReportBuilder
+    {
+        protected Report reportObject;
+        public abstract void SetReportType();
+        public abstract void SetReportHeader();
+        public abstract void SetReportContent();
+        public abstract void SetReportFooter();
+
+        public void CreateNewReport()
+        {
+            reportObject = new Report();
+        }
+
+        public Report GetReport()
+        {
+            return reportObject;
+        }
+    }
+
+    public class Report
+    {
+        public string ReportType { get; set; }
+        public string ReportHeader { get; set; }
+        public string ReportFooter { get; set; }
+        public string ReportContent { get; set; }
+
+        public void DisplayReport()
+        {
+            Console.WriteLine("Report Type :" + ReportType);
+            Console.WriteLine("Header :" + ReportHeader);
+            Console.WriteLine("Content :" + ReportContent);
+            Console.WriteLine("Footer :" + ReportFooter);
+        }
+    }
+
+    class ExcelReport : ReportBuilder
+    {
+        public override void SetReportContent()
+        {
+            reportObject.ReportContent = "Excel Content Section";
+        }
+        public override void SetReportFooter()
+        {
+            reportObject.ReportFooter = "Excel Footer";
+        }
+        public override void SetReportHeader()
+        {
+            reportObject.ReportHeader = "Excel Header";
+        }
+        public override void SetReportType()
+        {
+            reportObject.ReportType = "Excel";
+        }
+    }
+
+    public class PDFReport : ReportBuilder
+    {
+        public override void SetReportContent()
+        {
+            reportObject.ReportContent = "PDF Content Section";
+        }
+        public override void SetReportFooter()
+        {
+            reportObject.ReportFooter = "PDF Footer";
+        }
+        public override void SetReportHeader()
+        {
+            reportObject.ReportHeader = "PDF Header";
+        }
+        public override void SetReportType()
+        {
+            reportObject.ReportType = "PDF";
+        }
+    }
+
+    public class ReportDirector
+    {
+        public Report MakeReport(ReportBuilder reportBuilder)
+        {
+            reportBuilder.CreateNewReport();
+            reportBuilder.SetReportType();
+            reportBuilder.SetReportHeader();
+            reportBuilder.SetReportContent();
+            reportBuilder.SetReportFooter();
+            return reportBuilder.GetReport();
+        }
+    }
+}
+
+namespace DoFactory.GangOfFour.Builder.RealWorld
+{
+    /// <summary>
+    /// MainApp startup class for Real-World 
+    /// Builder Design Pattern.
+    /// </summary>
+    public class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        public static void Main()
+        {
+            VehicleBuilder builder;
+            // Create shop with vehicle builders
+            Shop shop = new Shop();
+            // Construct and display vehicles
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// The 'Director' class
+    /// </summary>
+    class Shop
+    {
+        // Builder uses a complex series of steps
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+    /// <summary>
+    /// The 'Builder' abstract class
+    /// </summary>
+    abstract class VehicleBuilder
+    {
+        protected Vehicle vehicle;
+        // Gets vehicle instance
+        public Vehicle Vehicle
+        {
+            get { return vehicle; }
+        }
+        // Abstract build methods
+        public abstract void BuildFrame();
+        public abstract void BuildEngine();
+        public abstract void BuildWheels();
+        public abstract void BuildDoors();
+    }
+    /// <summary>
+    /// The 'ConcreteBuilder1' class
+    /// </summary>
+    class MotorCycleBuilder : VehicleBuilder
+    {
+        public MotorCycleBuilder()
+        {
+            vehicle = new Vehicle("MotorCycle");
+        }
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "MotorCycle Frame";
+        }
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "500 cc";
+        }
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+    }
+    /// <summary>
+    /// The 'ConcreteBuilder2' class
+    /// </summary>
+    class CarBuilder : VehicleBuilder
+    {
+        public CarBuilder()
+        {
+            vehicle = new Vehicle("Car");
+        }
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Car Frame";
+        }
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "2500 cc";
+        }
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "4";
+        }
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "4";
+        }
+    }
+    /// <summary>
+    /// The 'ConcreteBuilder3' class
+    /// </summary>
+    class ScooterBuilder : VehicleBuilder
+    {
+        public ScooterBuilder()
+        {
+            vehicle = new Vehicle("Scooter");
+        }
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Scooter Frame";
+        }
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "50 cc";
+        }
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+    }
+    /// <summary>
+    /// The 'Product' class
+    /// </summary>
+    class Vehicle
+    {
+        private string _vehicleType;
+        private Dictionary<string, string> _parts = new Dictionary<string, string>();
+        // Constructor
+        public Vehicle(string vehicleType)
+        {
+            this._vehicleType = vehicleType;
+        }
+        // Indexer
+        public string this[string key]
+        {
+            get { return _parts[key]; }
+            set { _parts[key] = value; }
+        }
+        public void Show()
+        {
+            Console.WriteLine("\n---------------------------");
+            Console.WriteLine("Vehicle Type: {0}", _vehicleType);
+            Console.WriteLine(" Frame : {0}", _parts["frame"]);
+            Console.WriteLine(" Engine : {0}", _parts["engine"]);
+            Console.WriteLine(" #Wheels: {0}", _parts["wheels"]);
+            Console.WriteLine(" #Doors : {0}", _parts["doors"]);
+        }
+    }
+}
